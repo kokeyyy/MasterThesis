8c5e5de0c6085d1e38041038400e00557e8caf59
kokeyyy <96772978+kokeyyy@users.noreply.github.com>
2023-12-02 06:01:01 +0900

    Update lstm_seq2seq.py

--- lstm_seq2seq/lstm_seq2seq.py
+++ lstm_seq2seq/lstm_seq2seq.py
@@ -55,7 +55,7 @@ class LSTM_seq2seq(nn.Module):
 
         # define LSTM layer
         self.encoder_lstm = nn.LSTM(input_size = 10, hidden_size = hidden_size, num_layers = num_layers, batch_first=True, dropout = dropout)
-        self.decoder_lstm = nn.LSTM(input_size = num_features, hidden_size = hidden_size, num_layers = num_layers, batch_first=True, dropout = dropout)
+        self.decoder_lstm = nn.LSTM(input_size = 10, hidden_size = hidden_size, num_layers = num_layers, batch_first=True, dropout = dropout)
         # define fc layer for encoder
         self.linear = nn.Linear(num_features, 256)
         self.linear2 = nn.Linear(256, 10)
