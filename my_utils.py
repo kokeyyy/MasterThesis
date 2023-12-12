ec4275160a255a02c9216667bd2e6343cd393b26
kokeyyy <96772978+kokeyyy@users.noreply.github.com>
2023-12-13 07:49:04 +0900

    Update my_utils.py

--- my_utils.py
+++ my_utils.py
@@ -70,7 +70,7 @@ def plot_population(y_true, y_pred, title='Results', flag='plot', anomaly_dateti
         fig.update_yaxes(title='Population', showgrid=False, row=i+1, col=1)
 
         if flag == 'anomaly_detection':
-            for t in zip(anomaly_datetime[i]):
+            for t in anomaly_datetime[i]:
                 fig.add_vrect(
                             x0=t - datetime.timedelta(hours=1),
                             x1=t + datetime.timedelta(hours=1),
